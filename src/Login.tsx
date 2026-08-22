9b0215b6f801d5490064acd4c319aef928aaf8d1
HenryHYeary <HenryHYeary@github.com>
2026-08-22 14:17:07 -0500

    Add dropbox svg and beginnings of OAuth button in auth route.

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,9 +1,16 @@
+import DropboxLogo from "./assets/dropbox-color-svgrepo-com.svg"
+
 export default function Login() {
   return (
     <>
+    <div className="root-container">
       <div className="hero">
         <h1>Sign In via Dropbox</h1>
       </div>
+      <button className="sign-in-btn">
+        <img src={DropboxLogo} height={20} width={20} alt="Dropbox Icon" /> Authenticate via Dropbox
+      </button>
+    </div>
     </>
   )
 }
