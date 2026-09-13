2e5b5566da331722d50d7b63d96aa204adb1af94
HenryHYeary <henmofb@yahoo.com>
2026-09-12 20:42:37 -0500

    Add ability to remove extension chips in Editor element.

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,13 +1,24 @@
-const CURRENT_FOLDER = "~/.config/retroarch/saves";
-const WATCHED_TAG = "saves"
+import { useState } from "react";
 
-const DUMMY_DATA =  {
-  label: "saves",
-  path: "~/.config/retroarch/saves",
-  watchedExts: [".srm", ".save", ".sav"]
-};
+const CURRENT_FOLDER = "~/.config/retroarch/saves";
+const WATCHED_TAG = "saves";
 
 export default function Editor() {
+  const [exts, setExts] = useState([
+    { id: 1, name: ".srm" },
+    { id: 2, name: ".save" },
+    { id: 3, name: ".sav" },
+  ]);
+  const DUMMY_DATA =  {
+    label: "saves",
+    path: "~/.config/retroarch/saves",
+    watchedExts: exts
+  };
+
+  const handleRemove = (id: number) => {
+    setExts(prevExts => prevExts.filter(item => item.id !== id));
+  }
+
   return (
     <div className="hero">
       <h1>Editing Watched Directory: <span className="watched-directory-editor">{CURRENT_FOLDER}</span></h1>
@@ -16,7 +27,7 @@ export default function Editor() {
       <h2>Watched extensions:</h2>
       <ul className="extension-list-btns">
         {DUMMY_DATA.watchedExts.map((ext) => {
-          return <button className="ext-btn">{ext}</button>
+          return <button className="ext-btn">{ext.name}<span onClick={() => handleRemove(ext.id)} className="clickable-x">x</span></button>
         })}
       </ul>
     </div>
