a62e8cc7154f216681160e9f0274dc0b12076cd8
HenryHYeary <henmofb@yahoo.com>
2026-09-12 20:12:52 -0500

    Add more to Editor element.

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,11 +1,24 @@
 const CURRENT_FOLDER = "~/.config/retroarch/saves";
 const WATCHED_TAG = "saves"
 
+const DUMMY_DATA =  {
+  label: "saves",
+  path: "~/.config/retroarch/saves",
+  watchedExts: [".srm", ".save", ".sav"]
+};
+
 export default function Editor() {
   return (
-    <div>
-      <h1>Editing Watched Directory: {CURRENT_FOLDER}</h1>
-      <h1>Tag of Watched Directory: {WATCHED_TAG}</h1>
+    <div className="hero">
+      <h1>Editing Watched Directory: <span className="watched-directory-editor">{CURRENT_FOLDER}</span></h1>
+      <h1>Tag of Watched Directory: <span className="watched-tag-editor">{WATCHED_TAG}</span></h1>
+
+      <h2>Watched extensions:</h2>
+      <ul className="extension-list-btns">
+        {DUMMY_DATA.watchedExts.map((ext) => {
+          return <button className="ext-btn">{ext}</button>
+        })}
+      </ul>
     </div>
   )
 }
